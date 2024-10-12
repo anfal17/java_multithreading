12a41ca54324a7070b8b07548e7b12bb99a5f981
anfal17 <mohdanfal17@gmail.com>
2024-10-12 15:17:25 +0530

    init

---
+++ Test.java
@@ -0,0 +1,11 @@
+//main thread is the first thread
+
+/**
+ * Test
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
